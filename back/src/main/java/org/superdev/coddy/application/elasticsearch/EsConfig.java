3bd68ff51a5a8b8eb346627e794901770a5f9298
Augustin Husson <husson.augustin@gmail.com>
2017-07-01 22:43:03 +0200

    remove cluster name in EsConfig

--- back/src/main/java/org/superdev/coddy/application/elasticsearch/EsConfig.java
+++ back/src/main/java/org/superdev/coddy/application/elasticsearch/EsConfig.java
@@ -3,7 +3,6 @@ package org.superdev.coddy.application.elasticsearch;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,19 +23,11 @@ public class EsConfig {
     @Value("${elasticsearch.port}")
     private int esPort;
 
-    @Value("${elasticsearch.clustername}")
-    private String esClusterName;
-
     @Bean
     public Client client() throws Exception {
 
-        Settings esSettings = Settings.settingsBuilder()
-                .put("cluster.name", esClusterName)
-                .build();
-
         //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
         return TransportClient.builder()
-                .settings(esSettings)
                 .build()
                 .addTransportAddress(
                         new InetSocketTransportAddress(InetAddress.getByName(esHost), esPort));
