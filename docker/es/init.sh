7856ae6c96d0e9f13bed0d074b17e249732bd009
Augustin Husson <husson.augustin@gmail.com>
2017-07-02 20:21:01 +0200

    insert mapping account in docker and elasticsearch at the end

--- docker/es/init.sh
+++ docker/es/init.sh
@@ -2,6 +2,10 @@
 
 bash /docker-entrypoint.sh elasticsearch & sleep 10s
 
-cd / && curl -XPOST http://localhost:9200/_bulk --data-binary @user.bulk;
+ACCOUNT_INDEX=account
+
+cd / \
+&& curl -XPUT http://localhost:9200/${ACCOUNT_INDEX} -d @${ACCOUNT_INDEX}.json \
+&& curl -XPOST http://localhost:9200/_bulk --data-binary @user.bulk;
 
 wait
