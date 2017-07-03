36e4377e276644b573923263622ebaecf1bc396f
Augustin Husson <husson.augustin@gmail.com>
2017-07-04 00:57:18 +0200

    build war package only if the project is found otherwise do nothing

--- docker/back/entrypoint.sh
+++ docker/back/entrypoint.sh
@@ -2,10 +2,9 @@
 
 if [[ ! -d "$PROJECT_DIRECTORY" ]]; then
     echo "directory $PROJECT_DIRECTORY doesn't exist, unable to create the war package"
-    exit 1
+else
+    cd ${PROJECT_DIRECTORY}/back && mvn --batch-mode clean package -Dmaven.test.skip=true
 fi
-
-cd ${PROJECT_DIRECTORY}/back && mvn --batch-mode clean package -Dmaven.test.skip=true
 
 # Execute every command from user or from CMD Docker directive
 exec "$@"
