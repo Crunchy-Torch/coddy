8b4adf2634064db0ea4cd84bd75c58b3e55aa4dc
DavidLevayer <david.levayer@gmail.com>
2017-07-16 20:16:59 +0200

    Add link to dashboard, update css

--- front/src/src/template/private/private.component.ts
+++ front/src/src/template/private/private.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-private',
-    templateUrl: 'private.component.html'
+    templateUrl: 'private.component.html',
+    styleUrls: ['private.component.css']
 })
 
 export class PrivateComponent {
