acb5b085b18a73e743837760de5f16f5165099f2
Augustin Husson <husson.augustin@gmail.com>
2017-07-22 17:14:53 +0200

    add utils method do get object from json files

--- back/src/test/java/org/superdev/coddy/application/utils/TestUtils.java
+++ back/src/test/java/org/superdev/coddy/application/utils/TestUtils.java
@@ -1,5 +1,10 @@
 package org.superdev.coddy.application.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class TestUtils {
 
     private static final String SERVER_HOST = "localhost";
@@ -14,6 +19,28 @@ public class TestUtils {
 
     public static String getUrl(final String endpoint) {
         return "http://" + SERVER_HOST + ":" + SERVER_PORT + API_PREFIX + endpoint;
+    }
+
+    /**
+     * Method to deserialize JSON content from given json file into given Java type.
+     *
+     * @param jsonFile the json file to read
+     * @param object   the object mapped to the json file
+     * @param <T>      the type of the object
+     * @return an object instance by the data contains inside the json file
+     * @throws IOException if the file doesn't exist
+     */
+    public static <T> T getObjecFromJson(final String jsonFile, Class<T> object) throws IOException {
+        InputStream stream = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            stream = TestUtils.class.getClassLoader().getResourceAsStream(jsonFile);
+            return mapper.readValue(stream, object);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
     }
 
 }
