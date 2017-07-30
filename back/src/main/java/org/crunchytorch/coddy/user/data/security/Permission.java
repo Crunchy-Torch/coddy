62cca7778147616a7d089ce4b3864ff38aeb9945
Augustin Husson <husson.augustin@gmail.com>
2017-07-30 21:20:57 +0200

    add javadoc to the permission

--- back/src/main/java/org/crunchytorch/coddy/user/data/security/Permission.java
+++ back/src/main/java/org/crunchytorch/coddy/user/data/security/Permission.java
@@ -6,8 +6,14 @@ public class Permission {
 
     public static final String MODERATION = "MODERATION";
 
+    /**
+     * permission to set when we need to protect a user account access.
+     */
     public static final String PERSO_ACCOUNT = "personal_account";
 
+    /**
+     * permission to set when we need to protect any modifications to a private snippet.
+     */
     public static final String PERSO_SNIPPET = "personal_snippet";
 
     private Permission(){
