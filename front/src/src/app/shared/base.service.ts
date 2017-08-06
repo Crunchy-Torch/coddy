566769a68e8bd20a93b078bfb8a0cae2a173a48c
Augustin Husson <husson.augustin@gmail.com>
2017-08-06 11:18:18 +0200

    add util method

--- front/src/src/app/shared/base.service.ts
+++ front/src/src/app/shared/base.service.ts
@@ -1,6 +1,7 @@
 import { Error } from './error/error';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
+import serverUrl from '../../conf';
 
 export class BaseService {
 
@@ -18,5 +19,9 @@ export class BaseService {
         console.log(res);
         let error: Error = new Error(res.status, res.statusText || 'Something went horribly wrong...');
         return Observable.throw(error);
+    }
+
+    protected buildUrl(endpoint: string): string{
+        return serverUrl + endpoint;
     }
 }
