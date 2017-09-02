520f31578eee23d2faa716c50e1c8bc061aac5ea
DavidL <david.levayer@gmail.com>
2017-09-02 16:55:05 +0200

    Switch from css to scss

--- front/src/src/app/core/template/private/private.component.ts
+++ front/src/src/app/core/template/private/private.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-private',
     templateUrl: 'private.component.html',
-    styleUrls: ['private.component.css']
+    styleUrls: ['private.component.scss']
 })
 
 export class PrivateComponent {
