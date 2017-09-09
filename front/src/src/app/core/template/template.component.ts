0620b5740cf689d30fcafa48e0c6187d51519d6a
DavidLevayer <david.levayer@gmail.com>
2017-09-09 15:37:08 +0200

    Fix container issue

--- front/src/src/app/core/template/template.component.ts
+++ front/src/src/app/core/template/template.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template',
-  templateUrl: 'template.component.html'
+  templateUrl: 'template.component.html',
+  styleUrls: ['template.component.scss']
 })
 
 export class TemplateComponent implements OnInit {
