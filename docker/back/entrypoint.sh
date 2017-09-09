b866e37286fc39a5d3217a138eeaf026f36ab2d5
Augustin Husson <husson.augustin@gmail.com>
2017-09-09 15:00:15 +0200

    cache dependencies and use local gradle to create executable jar

--- docker/back/entrypoint.sh
+++ docker/back/entrypoint.sh
@@ -3,7 +3,7 @@
 if [[ ! -d "$PROJECT_DIRECTORY" ]]; then
     echo "directory $PROJECT_DIRECTORY doesn't exist, unable to create the war package"
 else
-    cd ${PROJECT_DIRECTORY}/back && mvn --batch-mode clean package -Dmaven.test.skip=true
+    cd ${PROJECT_DIRECTORY}/back && GRADLE_USER_HOME=gradle_cache ./gradlew build -x test --parallel
 fi
 
 # Execute every command from user or from CMD Docker directive
