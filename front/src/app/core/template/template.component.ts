94a126f8d15584736dac55cc698bc43c0197a56a
DavidLevayer <david.levayer@gmail.com>
2017-09-13 13:52:11 +0200

    Fix template issue (conflict with container)

--- front/src/app/core/template/template.component.ts
+++ front/src/app/core/template/template.component.ts
@@ -5,8 +5,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template',
-  templateUrl: 'template.component.html',
-  styleUrls: ['template.component.scss']
+  templateUrl: 'template.component.html'
 })
 
 export class TemplateComponent implements OnInit {
