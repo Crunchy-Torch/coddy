de302bfe9d5282c7e87228b1c44087016c310a97
DavidLevayer <david.levayer@gmail.com>
2017-09-15 21:28:23 +0200

    Initiate new title component

---
+++ front/src/app/shared/title/title.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: 'title.component.html',
+  styleUrls: ['title.component.scss']
+})
+
+export class TitleComponent {
+
+  @Input()
+  text: string;
+
+  constructor() { }
+}
