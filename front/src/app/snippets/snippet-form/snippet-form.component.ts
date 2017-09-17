c8195703258bcbfc7c584d40971e0c0273a9189c
DavidLevayer <david.levayer@gmail.com>
2017-09-17 10:41:42 +0200

    Fix css --> scss

--- front/src/app/snippets/snippet-form/snippet-form.component.ts
+++ front/src/app/snippets/snippet-form/snippet-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-snippet-form',
   templateUrl: './snippet-form.component.html',
-  styleUrls: ['./snippet-form.component.css']
+  styleUrls: ['./snippet-form.component.scss']
 })
 export class SnippetFormComponent implements OnInit {
 
