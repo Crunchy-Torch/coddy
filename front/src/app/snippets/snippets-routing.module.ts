918fb389c22f4a1f41a13fb751cd0209d9587033
DavidLevayer <david.levayer@gmail.com>
2017-09-17 14:04:39 +0200

    Make route private

--- front/src/app/snippets/snippets-routing.module.ts
+++ front/src/app/snippets/snippets-routing.module.ts
@@ -1,10 +1,11 @@
+import { PrivateGuard } from '../core/guard/private.guard';
 import { SnippetFormComponent } from './snippet-form/snippet-form.component';
 import { SnippetComponent } from './snippet/snippet.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const SNIPPETS_ROUTES: Routes = [
-  { path: 'snippet/create', component: SnippetFormComponent },
+  { path: 'snippet/create', component: SnippetFormComponent, canActivate: [PrivateGuard] },
   { path: 'snippet/:id', component: SnippetComponent }
 ];
 
