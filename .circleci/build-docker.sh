cffbddf651b56e9ee02dd1916d1830599912fd69
Augustin Husson <husson.augustin@gmail.com>
2018-01-06 10:55:51 +0100

    provide default tag and new method to initialize a connection to the registry

--- .circleci/build-docker.sh
+++ .circleci/build-docker.sh
@@ -3,6 +3,15 @@
 l_coddy_path=$PWD
 l_docker_image_base_name=crunchytorch/coddy
 
+connect(){
+    docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+
+    if [ $? != 0 ]; then
+        echo "something wrong with the docker's command"
+        exit 1
+    fi
+}
+
 tag(){
     l_initial_tag=$1
     l_target_tag=$2
@@ -13,11 +22,25 @@ tag(){
 push(){
     l_initial_tag=$1
 
+    if [ -z "${l_initial_tag}" ]; then
+        l_initial_tag="dev-master"
+    fi
+
     docker push ${l_docker_image_base_name}:${l_initial_tag}
+
+    if [ $? != 0 ]; then
+        echo "something wrong with the docker's command"
+        exit 1
+    fi
 }
 
 build(){
     l_initial_tag=$1
+
+    if [ -z "${l_initial_tag}" ]; then
+        l_initial_tag="dev-master"
+    fi
+
     # ckeck that all front files exist
     if [ ! -d "front/dist" ]; then
         echo "the directory front/dist doesn't exist"
@@ -41,24 +64,29 @@ build(){
 function help {
     echo "-p or --push to push a docker image to the registry
 -b or --build to build a docker image
+-c or --connect to connect to the registry
 -t or --tag to tag a docker image from another docker image"
 }
 
 
 case $1 in
+    -c|--connect)
+    connect
+    shift
+    ;;
     -p|--push)
     push $2
-    shift # past argument=value
+    shift
     ;;
     -b|--build)
     build $2
-    shift # past argument=value
+    shift
     ;;
     -t|--tag)
     tag $2 $3
-    shift # past argument=value
+    shift
     ;;
     *)
-    help  # unknown option
+    help
     ;;
 esac
