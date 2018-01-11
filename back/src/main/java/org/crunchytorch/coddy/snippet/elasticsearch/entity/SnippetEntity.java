7e022ba902ef71ce0e710f26ecdfb451df86685b
DavidLevayer <david.levayer@gmail.com>
2018-01-11 13:37:26 +0100

    Add Serializable to entities

--- back/src/main/java/org/crunchytorch/coddy/snippet/elasticsearch/entity/SnippetEntity.java
+++ back/src/main/java/org/crunchytorch/coddy/snippet/elasticsearch/entity/SnippetEntity.java
@@ -18,6 +18,8 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SnippetEntity implements Serializable {
 
+    private static final long serialVersionUID = -3019383984430293553L;
+
     @Id
     private String _id;
 
