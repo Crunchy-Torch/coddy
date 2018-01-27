8d095062fa41638ed160b046edbb50594aec92e6
DavidLevayer <david.levayer@gmail.com>
2018-01-27 18:35:36 +0100

    Externalize (function not authorized in annotations) + fix regex to include port number

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
-import {JwtModule} from '@auth0/angular-jwt';
-import {Token} from './token';
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
+import { Token } from './token';
 import { TokenService } from './token.service';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginService } from './login/login.service';
@@ -12,8 +12,8 @@ export function tokenGetter() {
   return localStorage.getItem(Token.TOKEN_KEY);
 }
 
-function getHostName(url) {
-  const match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+export function getHostName(url) {
+  const match = url.match(/:\/\/(www[0-9]?\.)?(.[^/]+)/i);
   if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
     return match[2];
   }
@@ -22,15 +22,22 @@ function getHostName(url) {
   }
 }
 
+export function jwtOptionsFactory() {
+  return {
+    tokenGetter: tokenGetter,
+    throwNoTokenError: false,
+    whitelistedDomains: [getHostName(environment.serverUrl)]
+  }
+}
+
 @NgModule({
   imports: [
     SharedModule,
     AuthRoutingModule,
     JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        throwNoTokenError: false,
-        whitelistedDomains: [getHostName(environment.serverUrl)]
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory
       }
     })
   ],
