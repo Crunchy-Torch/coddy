8e1549f3985caa5ca35de839f638cbf00b6a491d
Augustin Husson <husson.augustin@gmail.com>
2018-02-17 18:26:59 +0100

    add Page as DTO

---
+++ back/src/main/java/org/crunchytorch/coddy/application/data/Page.java
@@ -0,0 +1,39 @@
+package org.crunchytorch.coddy.application.data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page<T> implements Serializable {
+
+    private static final long serialVersionUID = -808950184664034440L;
+
+    private long totalElement;
+
+    private long totalPage;
+
+    private List<T> hits;
+
+    public Page() {
+        // this blank constructor is needed by the library jackson
+    }
+
+    public Page(org.springframework.data.domain.Page<T> page) {
+        this.totalElement = page.getTotalElements();
+        this.totalPage = page.getTotalPages();
+        this.hits = new ArrayList<>();
+        page.forEach(hits::add);
+    }
+
+    public long getTotalElement() {
+        return totalElement;
+    }
+
+    public long getTotalPage() {
+        return totalPage;
+    }
+
+    public List<T> getHits() {
+        return hits;
+    }
+}
