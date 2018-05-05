20cf58f813e495bb665579cffdebf2e1bb77f6d0
Augustin Husson <husson.augustin@gmail.com>
2018-05-05 17:32:35 +0200

    add MediaType since spring doesn't provide a class with such constant

---
+++ back/src/main/java/org/crunchytorch/coddy/application/data/MediaType.java
@@ -0,0 +1,10 @@
+package org.crunchytorch.coddy.application.data;
+
+public class MediaType {
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    private MediaType(){
+
+    }
+}
