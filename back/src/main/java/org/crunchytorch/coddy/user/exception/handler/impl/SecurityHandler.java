70d236a93a7016cd688cfb582d539ba4e097c38b
Augustin Husson <husson.augustin@gmail.com>
2018-05-07 11:53:18 +0200

    set order in controllerAdvice

--- back/src/main/java/org/crunchytorch/coddy/user/exception/handler/impl/SecurityHandler.java
+++ back/src/main/java/org/crunchytorch/coddy/user/exception/handler/impl/SecurityHandler.java
@@ -5,12 +5,15 @@ import org.crunchytorch.coddy.application.exception.handler.IExceptionHandler;
 import org.crunchytorch.coddy.user.exception.SecurityException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SecurityHandler implements IExceptionHandler<SecurityException> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SecurityHandler.class);
