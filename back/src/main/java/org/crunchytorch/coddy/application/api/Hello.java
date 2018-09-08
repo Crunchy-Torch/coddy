fb2e1dfdac8a78c541d10b626555acde7975dfc1
Augustin Husson <husson.augustin@gmail.com>
2018-09-08 18:06:01 +0200

    remove unused import

--- back/src/main/java/org/crunchytorch/coddy/application/api/Hello.java
+++ back/src/main/java/org/crunchytorch/coddy/application/api/Hello.java
@@ -3,7 +3,6 @@ package org.crunchytorch.coddy.application.api;
 import org.crunchytorch.coddy.application.data.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
